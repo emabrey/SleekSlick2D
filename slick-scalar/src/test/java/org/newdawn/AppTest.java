acadcc6dea1c0a242fed41f9a6a8f667c2586fda
Emily Mabrey <emilymabrey93+github@gmail.com>
2013-09-18 02:50:08 -0400

    Port Slick2D build 237 to Maven.

---
+++ slick-scalar/src/test/java/org/newdawn/AppTest.java
@@ -0,0 +1,17 @@
+package org.newdawn;
+
+import org.testng.annotations.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+@Test(groups = {"UnitTest"})
+public class AppTest {
+
+    @Test
+    public void AppTest() {
+
+    }
+
+}
